e7e48f9d64c798215e227d4603ed69b5c6cf1e84
talvinramnah <73443744+talvinramnah@users.noreply.github.com>
2025-06-05 15:07:02 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["imgur.com", "i.imgur.com","i.imgbb.com","i.ili.io","flic.kr"],
+    domains: ["imgur.com", "i.imgur.com","i.imgbb.com","i.ili.io","flic.kr","live.staticflickr.com"],
   },
 };
 
